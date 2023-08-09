bfbce6d33ac1179a4d9dc770ba14bd49c13b915e
Stewart Wingfield <sww915@gmail.com>
2023-08-08 21:39:10 -0400

    button click is a toggle now

--- main.js
+++ main.js
@@ -23,25 +23,28 @@ const displayUser = () => {
       const li = document.createElement('li')
       const text = document.createTextNode(`${user.name.first} ${user.name.last}`)
       const image = document.createElement('img')
+      
       image.src = user.picture.thumbnail
       li.appendChild(text)
       li.appendChild(image)
       allUsers.append(li)
+      let userDetails = document.createElement('h4')
+      userDetails.className='hide'
+        userDetails.textContent = `Cell: ${user.cell} DOB: ${Object.entries(user.dob)} Email: ${user.email}, Gender: ${user.gender}, ID: ${Object.entries(user.id)}, Location: ${user.location.city}, ${user.location.country} Login: ${Object.entries(user.login)}, NAT: ${user.nat}, Phone: ${user.phone}, Registered: ${Object.entries(user.registered)}, Phone: ${user.phone}`
+        
+        li.appendChild(userDetails)
 
 
       const btn = document.createElement('button')
       btn.innerHTML = "Show rest of info"
-
+      btn.addEventListener('click', function(){
+        userDetails.classList.toggle('hide')
+      })
       li.appendChild(btn)
-
-      showDetails = () => {
-        const userDetails = document.createElement('h4')
-        userDetails.textContent = `Cell: ${user.cell}, DOB: ${Object.entries(user.dob)}, Email: ${user.email}, Gender: ${user.gender}, ID: ${Object.entries(user.id)}, Location: ${user.location.city}, ${user.location.country} Login: ${Object.entries(user.login)}, NAT: ${user.nat}, Phone: ${user.phone}, Registered: ${Object.entries(user.registered)}, Phone: ${user.phone}`
-        li.appendChild(userDetails)
-        btn.removeEventListener('click', showDetails)
-        }
-    
-        btn.addEventListener('click', showDetails)
+        
+        
     })
 }
+    
+    
 
